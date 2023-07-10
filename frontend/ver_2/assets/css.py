90d50056ad331f2b7ab284c81d03f8142750d061
osmin625 <tmdals179@gmail.com>
2023-07-10 18:21:36 +0000

    feat/update sidebar design

--- frontend/ver_2/assets/css.py
+++ frontend/ver_2/assets/css.py
@@ -2,6 +2,7 @@ HEADER_STYLE = {
     'height': '7vh',
     'background-color':'#6690ff',
     'display':'flex',
+    '--bs-navbar-padding-y': '0rem',
 }
 
 SIDEBAR_STYLE = {
@@ -23,15 +24,17 @@ SIDEBAR_HIDDEN = {
 # the styles for the main content position it to the right of the sidebar and
 # add some padding.
 CONTENT_SIDE_ON = {
-    "padding": "2rem 1rem",
+    # "padding": "2rem 1rem",
     'width':'85vw',
+    "height": "93vh", 
     "transition": "all 0.5s",
     "background-color": "#f8f9fa",
 }
 
 CONTENT_SIDE_OFF = {
-    "padding": "2rem 1rem",
-    'width':'95vw',
+    # "padding": "2rem 1rem",
+    'width':'96vw',
+    "height": "93vh", 
     "transition": "all 0.5s",
     "background-color": "#f8f9fa",
 }
@@ -40,4 +43,21 @@ NAVSTYLE = {
     'color':'#aeaeec',
     'transition-delay':'all 0.5s',
     '--bs-nav-link-padding-x': '0.4rem',
+    
+}
+
+HEADER_BOX_SHOW = {
+    'background-color': '#1e22fe',
+    'height': '100%',
+    'width': '15vw',
+    "transition": "all 0.5s",
+    'display':'flex',
+}
+
+HEADER_BOX_HIDDEN = {
+    'background-color': '#1e22fe',
+    'height': '100%',
+    'width': '4vw',
+    "transition": "all 0.5s",
+    'display':'flex',
 }
