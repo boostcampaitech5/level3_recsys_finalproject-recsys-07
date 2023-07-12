513ad77d3bada338df7391377992a30c88012b5f
osmin625 <tmdals179@gmail.com>
2023-07-12 12:42:31 +0000

    feat: add figs into dashboard #24

---
+++ frontend/ver_2/assets/figure.py
@@ -0,0 +1,46 @@
+import plotly.express as px
+import plotly.graph_objects as go
+
+
+def draw_pie_chart(df, column, min_range=0, max_range=50):
+    """
+    column : 출력 원하는 column명
+    min_range, max_range : 함수에 표시할 sentence_index 범위, 기본값 [0, 50]
+    """
+    df = (
+        df[[column]]
+        .loc[(df["sentence_index"] >= min_range) & (df["sentence_index"] <= max_range)]
+        .value_counts()
+        .to_frame()
+    )
+    df.columns = ["count"]
+    df.reset_index(inplace=True)
+    if df.shape[0] > 10:
+        df.loc[9] = ["기타", df["count"].iloc[10:].sum()]
+        df = df.iloc[:10]
+    fig = go.Figure(data=[go.Pie(labels=df[column], values=df["count"])])
+    fig.update_traces(textinfo="percent")
+    fig.update_layout(title=f"{column} Distribution")
+    fig.update_layout(
+        paper_bgcolor="rgba(0,0,0,0)"
+    )  # figure에서는 테두리를 둥글게 만들 수 없어서 배경색을 투명하게 설정.
+    # fig.show()
+    return fig
+
+
+def draw_bar_chart(df, column, min_range=0, max_range=50, horizontal=False):
+    """
+    column : 출력 원하는 column명
+    min_range, max_range : 함수에 표시할 sentence_index 범위, 기본값 [0, 50]
+    horizontal : 가로 출력 여부
+    """
+    df = df[column].value_counts().to_frame().reset_index()
+    df.columns = [column, "count"]
+    if horizontal:
+        fig = px.bar(df, x="count", y=column, orientation="h")
+    else:
+        fig = px.bar(df, x=column, y="count")
+    fig.update_layout(
+        paper_bgcolor="rgba(0,0,0,0)"
+    )  # figure에서는 테두리를 둥글게 만들 수 없어서 배경색을 투명하게 설정.
+    return fig
