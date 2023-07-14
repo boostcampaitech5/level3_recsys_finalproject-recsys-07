d7e2f4c3ff1249641358540411cf0462709500fa
Oh seungmin <tmdals179@gmail.com>
2023-07-15 01:36:05 +0900

    Refactor: frontend (#39)
    * refactor: replace dashboard design
    * feat: add tailwind, replace python css style to tailwind css

--- frontend/ver_2/pages/overview.py
+++ frontend/ver_2/pages/overview.py
@@ -1,70 +1,51 @@
 import dash
 import pandas as pd
 from dash import html, dcc
-from assets import css
 from assets import figure
 
 
 dash.register_page(__name__)
 
-# the style arguments for the sidebar. We use position:fixed and a fixed width
-
-# the styles for the main content position it to the right of the sidebar and
-# add some padding.
-
 df = pd.read_csv("../../data/durecdial/dev_pp.csv")
-# print(df.columns)
 layout = html.Div(
     [
-        html.Div(
-            style={
-                "height": "7vh",
-            }
-        ),
         html.Div(
             children=[
                 html.Span(
                     children=[
-                        html.Div(
-                            className="bi bi-clipboard-data round", style=css.CARD_ICON
-                        ),
-                        html.Div(df.size, style=css.CARD_VALUE),
+                        html.Div(className="bi bi-clipboard-data round card-icon"),
+                        html.Div(df.size, className="card-value"),
                         html.Div("# of instances (rows)"),
                     ],
-                    style=css.FIGURE_CARD,
+                    className="figure-card",
                 ),
                 html.Span(
                     children=[
-                        html.Div(
-                            className="bi bi-people-fill round", style=css.CARD_ICON
-                        ),
-                        html.Div(children=[df.size], style=css.CARD_VALUE),
+                        html.Div(className="bi bi-people-fill round card-icon"),
+                        html.Div(children=[df.size], className="card-value"),
                         html.Div("# of users"),
                     ],
-                    style=css.FIGURE_CARD,
+                    className="figure-card",
                 ),
                 html.Span(
                     children=[
                         html.Div(
-                            className="bi bi-layout-three-columns round",
-                            style=css.CARD_ICON,
+                            className="bi bi-layout-three-columns round card-icon",
                         ),
-                        html.Div(len(df.columns), style=css.CARD_VALUE),
+                        html.Div(len(df.columns), className="card-value"),
                         html.Div("# of features (columns)"),
                     ],
-                    style=css.FIGURE_CARD,
+                    className="figure-card",
                 ),
                 html.Span(
                     children=[
-                        html.Div(
-                            className="bi bi-chat-left-dots round", style=css.CARD_ICON
-                        ),
+                        html.Div(className="bi bi-chat-left-dots round card-icon"),
                         html.Div("# of topics"),
                         html.Div(len(df["goal_topic"].unique())),
                         html.Div("# of goal_type"),
                         html.Div(len(df["goal_type"].unique())),
                     ],
-                    style=css.FIGURE_CARD,
+                    className="figure-card",
                 ),
             ],
             style={
@@ -79,11 +60,11 @@ layout = html.Div(
                     children=[
                         dcc.Graph(
                             figure=figure.draw_pie_chart(df, "goal_type"),
-                            style=css.FIG_STYLE_1,
+                            className="fig",
                         ),
                         dcc.Graph(
                             figure=figure.draw_pie_chart(df, "goal_topic"),
-                            style=css.FIG_STYLE_1,
+                            className="fig",
                         ),
                     ],
                     style={
@@ -95,17 +76,17 @@ layout = html.Div(
                     children=[
                         dcc.Graph(
                             figure=figure.draw_bar_chart(df, "wday"),
-                            style=css.FIG_STYLE_2,
+                            className="fig",
                         ),
                         html.Div(
                             children=[
                                 dcc.Graph(
                                     figure=figure.draw_pie_chart(df, "place"),
-                                    style=css.FIG_STYLE_3,
+                                    className="fig",
                                 ),
                                 dcc.Graph(
                                     figure=figure.draw_pie_chart(df, "topic"),
-                                    style=css.FIG_STYLE_3,
+                                    className="fig",
                                 ),
                             ],
                             style={
@@ -117,7 +98,7 @@ layout = html.Div(
                         ),
                         dcc.Graph(
                             figure=figure.draw_bar_chart(df, "time"),
-                            style=css.FIG_STYLE_2,
+                            className="fig",
                         ),
                     ],
                     style={
