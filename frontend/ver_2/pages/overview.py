9ef3b832ffca97a4790f67693237a5a01018863d
osmin625 <tmdals179@gmail.com>
2023-07-15 08:20:52 +0000

    feat: add overview page

--- frontend/ver_2/pages/overview.py
+++ frontend/ver_2/pages/overview.py
@@ -2,125 +2,110 @@ import dash
 import pandas as pd
 from dash import html, dcc
 from assets import figure
-import dash_bootstrap_components as dbc
 
 dash.register_page(__name__)
 
 df = pd.read_csv("../../data/durecdial/dev_pp.csv")
 layout = html.Div(
-    [
-        dbc.Navbar(
-            children=[],
-            className="header",
-        ),
+    children=[
         html.Div(
             children=[
                 html.Div(
                     children=[
-                        html.Span(
-                            children=[
-                                html.Div(
-                                    className="bi bi-clipboard-data round card-icon"
-                                ),
-                                html.Div(df.size, className="card-value"),
-                                html.Div("# of instances (rows)"),
-                            ],
-                            className="figure-card",
-                        ),
-                        html.Span(
-                            children=[
-                                html.Div(className="bi bi-people-fill round card-icon"),
-                                html.Div(children=[df.size], className="card-value"),
-                                html.Div("# of users"),
-                            ],
-                            className="figure-card",
-                        ),
-                        html.Span(
-                            children=[
-                                html.Div(
-                                    className="bi bi-layout-three-columns round card-icon",
-                                ),
-                                html.Div(len(df.columns), className="card-value"),
-                                html.Div("# of features (columns)"),
-                            ],
-                            className="figure-card",
-                        ),
-                        html.Span(
-                            children=[
-                                html.Div(
-                                    className="bi bi-chat-left-dots round card-icon"
-                                ),
-                                html.Div("# of topics"),
-                                html.Div(len(df["goal_topic"].unique())),
-                                html.Div("# of goal_type"),
-                                html.Div(len(df["goal_type"].unique())),
-                            ],
-                            className="figure-card",
-                        ),
+                        html.Div(className="bi bi-clipboard-data round card-icon"),
+                        html.Div(df.size, className="card-value"),
+                        html.Div("# of instances (rows)"),
                     ],
-                    style={
-                        "min-height": "22vh",
-                        "display": "flex",
-                        "justify-content": "space-around",
-                    },
+                    className="figure-card",
+                ),
+                html.Div(
+                    children=[
+                        html.Div(className="bi bi-people-fill round card-icon"),
+                        html.Div(children=[df.size], className="card-value"),
+                        html.Div("# of users"),
+                    ],
+                    className="figure-card",
                 ),
                 html.Div(
                     children=[
                         html.Div(
-                            children=[
-                                dcc.Graph(
-                                    figure=figure.draw_pie_chart(df, "goal_type"),
-                                    className="fig",
-                                ),
-                                dcc.Graph(
-                                    figure=figure.draw_pie_chart(df, "goal_topic"),
-                                    className="fig",
-                                ),
-                            ],
-                            style={
-                                "width": "40%",
-                                "height": "100%",
-                            },
+                            className="bi bi-layout-three-columns round card-icon",
                         ),
-                        html.Div(
-                            children=[
-                                dcc.Graph(
-                                    figure=figure.draw_bar_chart(df, "wday"),
-                                    className="fig",
-                                ),
-                                html.Div(
-                                    children=[
-                                        dcc.Graph(
-                                            figure=figure.draw_pie_chart(df, "place"),
-                                            className="fig",
-                                        ),
-                                        dcc.Graph(
-                                            figure=figure.draw_pie_chart(df, "topic"),
-                                            className="fig",
-                                        ),
-                                    ],
-                                    style={
-                                        # "display": "flex",
-                                        # "display-direction": "row",
-                                        # "min-width": "100%",
-                                        # 'min-heigh':'100%',
-                                    },
-                                ),
-                                dcc.Graph(
-                                    figure=figure.draw_bar_chart(df, "time"),
-                                    className="fig",
-                                ),
-                                dcc.Graph(
-                                    figure=figure.draw_line_chart(df, "goal_type"),
-                                    className="fig",
-                                ),
-                            ],
-                        ),
+                        html.Div(len(df.columns), className="card-value"),
+                        html.Div("# of features (columns)"),
                     ],
+                    className="figure-card",
+                ),
+                html.Div(
+                    children=[
+                        html.Div(className="bi bi-chat-left-dots round card-icon"),
+                        html.Div("# of topics"),
+                        html.Div(len(df["goal_topic"].unique())),
+                        html.Div("# of goal_type"),
+                        html.Div(len(df["goal_type"].unique())),
+                    ],
+                    className="figure-card",
                 ),
             ],
-            className="content",
+            className="grid grid-cols-4",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    dcc.Graph(
+                        figure=figure.draw_line_chart(df, "goal_type"),
+                        className="fig",
+                    ),
+                    className="col-start-1 col-end-5",
+                ),
+                html.Div(
+                    dcc.Graph(
+                        figure=figure.draw_pie_chart(df, "goal_topic"),
+                        className="fig",
+                    ),
+                    className="col-start-5 col-end-7",
+                ),
+                html.Div(
+                    className="col-start-1 col-end-2 fig",
+                ),
+                html.Div(
+                    dcc.Graph(
+                        figure=figure.draw_pie_chart(df, "goal_type"),
+                        className="fig",
+                    ),
+                    className="col-start-2 col-end-4",
+                ),
+                html.Div(
+                    dcc.Graph(
+                        figure=figure.draw_bar_chart(df, "wday"),
+                        className="fig",
+                    ),
+                    className="col-start-4 col-end-7",
+                ),
+                html.Div(
+                    dcc.Graph(
+                        figure=figure.draw_pie_chart(df, "place"),
+                        className="fig",
+                    ),
+                    className="col-start-1 col-end-3",
+                ),
+                html.Div(
+                    dcc.Graph(
+                        figure=figure.draw_pie_chart(df, "topic"),
+                        className="fig",
+                    ),
+                    className="col-start-3 col-end-5",
+                ),
+                html.Div(
+                    dcc.Graph(
+                        figure=figure.draw_bar_chart(df, "time", horizontal=True),
+                        className="fig",
+                    ),
+                    className="col-start-5 col-end-7",
+                ),
+            ],
+            className="grid grid-cols-6 grid-rows-6",
         ),
     ],
-    className="flex flex-col",
+    className="content no-scrollbar flex flex-col",
 )
