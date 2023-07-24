58113986ab02628ccadde6110c0d0d17fc69142d
osmin625 <tmdals179@gmali.com>
2023-07-24 00:54:22 +0000

    feat: add components on model eval pages

--- frontend/pages/modelevaluation.py
+++ frontend/pages/modelevaluation.py
@@ -1,6 +1,6 @@
 import dash
 from dash import dash_table, html
-from assets.data import model_eval
+from assets.data import get_model_eval
 
 dash.register_page(__name__)
 
@@ -9,14 +9,15 @@ layout = html.Div(
         html.Div(
             [
                 html.Div(  # 소제목
-                    "• 모델 성능 확인하기",
+                    "• 모델 추천 성능",
                     className="title p-4",
                     style={"grid-area": "1 / 1 / span 1 / span 4"},
                 ),
                 html.Div(
                     [
                         dash_table.DataTable(
-                            data=model_eval.to_dict("records"),
+                            # Conversation, Recommendation
+                            data=get_model_eval("Recommendation"),
                             columns=[
                                 {"name": c, "id": c}
                                 for c in [
@@ -30,24 +31,10 @@ layout = html.Div(
                                     "NDCG@1",
                                     "NDCG@10",
                                     "NDCG@50",
-                                    "BLEU@1",
-                                    "BLEU@2",
-                                    "BLEU@3",
-                                    "BLEU@4",
-                                    "Dist@1",
-                                    "Dist@2",
-                                    "Dist@3",
-                                    "Dist@4",
-                                    "Average",
-                                    "Extreme",
-                                    "Greedy",
-                                    "PPL",
-                                    "Conversation",
-                                    "Policy",
-                                    "Recommendation",
                                 ]
                             ],
-                            id="models",
+                            id="rec-models",
+                            sort_action="native",
                             style_cell={
                                 "textAlign": "left",
                                 "padding": "10px 10px 10px 30px",
@@ -73,9 +60,69 @@ layout = html.Div(
                             page_size=10,
                         ),
                     ],
-                    style={"grid-area": "21 / 1 / span 17 / span 12"},
+                    style={"grid-area": "2 / 1 / span 10 / span 12"},
+                ),
+                html.Div(  # 소제목
+                    "• 모델 문장 생성 성능",
+                    className="title p-4",
+                    style={"grid-area": "12 / 1 / span 1 / span 4"},
+                ),
+                html.Div(
+                    [
+                        dash_table.DataTable(
+                            data=get_model_eval("Conversation"),
+                            columns=[
+                                {"name": c, "id": c}
+                                for c in [
+                                    "Model",
+                                    "BLEU@1",
+                                    "BLEU@2",
+                                    "BLEU@3",
+                                    "BLEU@4",
+                                    "Dist@1",
+                                    "Dist@2",
+                                    "Dist@3",
+                                    "Dist@4",
+                                    "Average",
+                                    "Extreme",
+                                    "Greedy",
+                                    "PPL",
+                                ]
+                            ],
+                            id="conv-models",
+                            sort_action="native",
+                            style_cell={
+                                "textAlign": "left",
+                                "padding": "10px 10px 10px 30px",
+                            },
+                            style_as_list_view=True,
+                            style_data={
+                                "whiteSpace": "normal",
+                                "color": "black",
+                                "backgroundColor": "white",
+                                "height": "auto",
+                            },
+                            style_data_conditional=[
+                                {
+                                    "if": {"row_index": "odd"},
+                                    "backgroundColor": "#f1f5f9",
+                                }
+                            ],
+                            style_header={
+                                "backgroundColor": "rgb(210, 210, 210)",
+                                "color": "black",
+                                "fontWeight": "bold",
+                            },
+                            page_size=10,
+                        ),
+                    ],
+                    style={
+                        # "grid-area": "13 / 1 / span 10 / span 12",
+                        "transition": "all 0.5s"
+                    },
                 ),
             ],
+            id="model-grid",
         ),
     ],
     className="content no-scrollbar",
