b7969ed8b72368b214d0adfb81905048dafca000
osmin625 <tmdals179@gmali.com>
2023-07-25 12:57:57 +0000

    refactor: header file update

--- frontend/assets/header.py
+++ frontend/assets/header.py
@@ -1,39 +1,41 @@
-import dash_bootstrap_components as dbc
 from dash import dcc, html
+import dash_bootstrap_components as dbc
 
-header = (
-    dbc.Navbar(
-        children=[
-            html.H1(
-                children=["Select Dataset🗃️ →"],
-                style={
-                    "margin-left": "3%",
-                    "font-weight": "bold",
-                    "font-size": "150%",
-                },
-            ),
-            dcc.Dropdown(
-                [
-                    {
-                        "label": html.Span(
-                            ["DuRecDial2.0 (sample)"],
-                            style={"font-size": 20},
-                        ),
-                        "value": "DuRecDial2.0",
+header = html.Div(
+    [
+        dbc.Navbar(
+            children=[
+                html.H1(
+                    children=["Select Dataset🗃️ →"],
+                    style={
+                        "margin-left": "3%",
+                        "font-weight": "bold",
+                        "font-size": "150%",
                     },
-                    {
-                        "label": html.Span(
-                            ["ReDial (sample)"], style={"font-size": 20}
-                        ),
-                        "value": "ReDial",
-                    },
-                ],
-                value="DuRecDial2.0",
-                className="data-selector",
-                optionHeight=50,
-            ),
-        ],
-        className="header",
-        id="header",
-    ),
+                ),
+                dcc.Dropdown(
+                    [
+                        {
+                            "label": html.Span(
+                                ["DuRecDial2.0 (sample)"],
+                                style={"font-size": 20},
+                            ),
+                            "value": "DuRecDial2.0",
+                        },
+                        {
+                            "label": html.Span(
+                                ["ReDial (sample)"], style={"font-size": 20}
+                            ),
+                            "value": "ReDial",
+                        },
+                    ],
+                    value="DuRecDial2.0",
+                    className="data-selector",
+                    optionHeight=50,
+                ),
+            ],
+            className="header",
+            id="header",
+        )
+    ]
 )
