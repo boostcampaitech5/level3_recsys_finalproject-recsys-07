045230e0fa01cf702436d31793dc5161f824610c
ji-yunkim <jiyunkim.study@gmail.com>
2023-07-26 09:19:49 +0000

    [feat] add README on data upload section

--- frontend/pages/readme.py
+++ frontend/pages/readme.py
@@ -1,32 +1,51 @@
 import dash
 from dash import html
+import dash_table
+import pandas as pd
 
 dash.register_page(__name__)
 
+df = pd.DataFrame(
+    [
+        ["user_id", "integer", "각 사용자의 고유번호","0"],
+        ["user_profile_age_range", "String", "사용자의 연령대 (임의설정 가능)","Over 50 years old"],
+        ["user_profile_name", "String", "사용자의 이름 (암호화 가능)","Fangyang Liu"],
+        ["user_profile_residence", "String","사용자의 거주지", "Qingdao"],
+        ["user_profile_accepted_food", "String", "사용자의 선호 음식", "Ziaoji stuffed with mackerel"],
+        ["user_profile_accepted_movies", "List of String","사용자의 선호 영화","['Left Right Love Destiny', 'Hot Summer Days', 'Fly Me to Polaris', 'Help!!!', 'One Night in Mongkok', 'The Bullet Vanishes']"],
+        ["user_profile_accepted_music", "List of String","사용자의 선호 음악","['Once']"],
+        ["user_profile_rejected_music", "List of String", "사용자의 불호 음악", "['Its Time']"],
+        ["user_profile_gender","String","사용자의 성별", "Female"],
+        ["user_profile_accepted_celebrities","List of String","사용자의 선호 유명인","['Cecilia Cheung', 'Kris Wu', 'Nicholas Tse']"],
+        ["user_profile_accepted_movie","String","사용자의 선호 영화","The Legend of Speed"],
+        ["user_profile_reject","String","사용자의 불호 매체","News"],
+        ["user_profile_rejected_movies","List of String","사용자의 불호 영화","['Everyday is Valentine', 'Unforgettable', 'King of Comedy']"],
+        ["user_profile_occupation","String","사용자의 직업 및 고용상태","Student"],
+        ["user_profile_accepted_poi","String","사용자가 선호 관심 지점","Minguo Seafood Dumpling House"],
+        ["user_profile_favorite_news","List of String","사용자가 가장 선호하는 뉴스기사","['Jacky Cheungs news']"],
+        ["user_profile_accepted_news","List of String","사용자의 선호 뉴스","['Leslie Cheungs news, Leehom Wangs news']"],
+        ["user_profile_poi","String","사용자가 밝힌 관심 지점","Mingyuan Spicy Hot Pot-Chongqing Roasted Fish"],
+        ["time","time","발화가 이루어진 시각","12:00:00 PM"],
+        ["place","String","발화가 이루어진 장소","at school"],
+        ["date","date","발화가 이루어진 날짜","2018-10-19"],
+        ["topic","String","발화의 상황 주제","Academic setback"],
+        ["wday","String","발화가 이루어진 요일","Wednesday"],
+        ["sentence_index","integer","한 대화 내 발화의 순번","0"],
+        ["sentence","String","발화 문장의 내용","Who is the leading actor of the movie Left Right Love Destiny?"],
+        ["goal_topic","String","발화 목적 키워드","Cecilia Cheung"],
+        ["goal_type","String","발화 목적","Chat about stars"],
+        ["knowledge","String","발화에 쓰인 배경지식 키워드","Cecilia Cheung, Stars, Left Right Love Destiny"]
+    ],
+    columns=["열 이름", "데이터 타입", "설명","예시"],
+)
+
+
 layout = html.Div(
     children=[
-        html.Img(
-            src="../assets/images/dashmon-high-resolution-color-logo.png",
-            style={"max-width": "20%"},
-        ),
-        html.H1(
-            children="Welcome to DASHMON!📊",
-            className="blink",
-            style={
-                "font-weight": "bold",
-                "margin-top": "3%",
-                "line-height": "150%",
-                "font-size": "200%",
-            },
-        ),
-        html.H2(
-            "DASHMON은 대화형 추천시스템에서 활용되는 데이터를 시각화하고 분석하는 모니터링 툴입니다.",
-            style={"line-height": "200%"},
-        ),
         html.Div(
             children=[
                 html.H3(
-                    children="💡 DASHMON의 목표",
+                    children="📤 로그 데이터 업로드",
                     style={
                         "font-weight": "bold",
                         "font-size": "150%",
@@ -36,15 +55,14 @@ layout = html.Div(
                 ),
                 html.H4(
                     children=[
-                        "대시몬은 추천이 포함된 대화형 데이터로부터 추천 성능에 관한 인사이트와 대화의 품질에 대한 인사이트를 동시에 제공하고자 합니다.",
+                        "DashMon에 대화형 추천시스템의 로그데이터를 업로드하여 정량적, 정성적으로 분석할 수 있습니다.",
                         html.Br(),
-                        "또한, 대시몬은 다양한 Baseline CRS 모델의 성능 비교를 제공하여 ",
-                        "Conversational Recommender System(CRS)을 개발하고 유지하는 데에 도움을 줍니다.",
+                        "DashMon을 효과적으로 이용하기 위해 아래와 같은 포맷을 지켜 업로드해주세요."
                     ],
                     style={"margin-left": "5%", "line-height": "200%"},
                 ),
                 html.H3(
-                    children="💡 DASHMON의 기능",
+                    children="📤 Dataset Format",
                     style={
                         "font-weight": "bold",
                         "font-size": "150%",
@@ -54,62 +72,22 @@ layout = html.Div(
                 ),
                 html.H4(
                     children=[
-                        "DASHMON은 크게 ",
                         html.A(
-                            "Overview", href="overview", className="text-lg font-bold"
+                            "예시 데이터셋 : DuRecDial2.0(EN)", 
+                            #href="../../data/durecdial/sample_data.csv",
+                            className="text-lg font-bold"
                         ),
-                        ", ",
-                        html.A(
-                            "Instance", href="instance", className="text-lg font-bold"
+                        html.Br(),
+                        " 데이터셋의 Feature(column)와 data type은 다음과 같습니다.",
+                        dash_table.DataTable(
+                            id="table",
+                            columns=[{"name": i, "id": i} for i in df.columns],
+                            data=df.to_dict("records"),
+                            #export_format="csv",
+                            style_table={'width':'80%'},
+                            style_header={'font-weight':'bold'},
+                            style_cell={'textAlign':'left'}
                         ),
-                        ", ",
-                        html.A(
-                            "Data Analysis",
-                            href="dataanalysis",
-                            className="text-lg font-bold",
-                        ),
-                        ", ",
-                        html.A(
-                            "Model Evaluation",
-                            href="modelevaluation",
-                            className="text-lg font-bold",
-                        ),
-                        " 네 페이지로 구성됩니다.",
-                        html.Br(),
-                        html.Br(),
-                        html.A(
-                            "Overview", href="overview", className="text-lg font-bold"
-                        ),
-                        "에서는 대상 데이터셋의 여러 속성들과 분포등을 확인할 수 있으며, ",
-                        "페이지 중앙에 위치한 Slider를 조정하여 대화 시점에 따른 데이터의 분포를 확인할 수 있습니다.",
-                        html.Br(),
-                        html.Br(),
-                        html.A(
-                            "Instance", href="instance", className="text-lg font-bold"
-                        ),
-                        "에서는 데이터셋을 직접 확인할 수 있습니다. ",
-                        "DuRecDial2.0 데이터셋의 경우 각 대화마다 User Profile 정보가 함께 주어지는데, ",
-                        "해당 내용을 편하게 확인할 수 있도록 유저 정보와 대화 정보를 구분하여 제공합니다. ",
-                        "또한 데이터를 확인할 때, 열을 선택할 수 있어 필요한 정보만 확인할 수 있습니다.",
-                        html.Br(),
-                        html.Br(),
-                        html.A(
-                            "Data Analysis",
-                            href="dataanalysis",
-                            className="text-lg font-bold",
-                        ),
-                        "에서는 Overview에서 얻지 못한 심층적인 정보를 얻을 수 있습니다. ",
-                        "데이터셋에 포함되는 추천의 효과 및 효율, 대화형 데이터의 사용 품질 등을 막대 그래프와 그래프 자료구조 시각화를 통해 확인할 수 있으며, ",
-                        "사용자 임베딩 시각화를 확인할 수 있습니다.",
-                        html.Br(),
-                        html.Br(),
-                        html.A(
-                            "Model Evaluation",
-                            href="modelevaluation",
-                            className="text-lg font-bold",
-                        ),
-                        "에서는 데이터셋을 CRS모델 학습에 활용했을 때의 정량적인 성능 지표를 확인할 수 있으며, "
-                        "지표에 따른 모델별 순위를 확인할 수 있습니다.",
                     ],
                     style={"margin-left": "5%", "line-height": "200%"},
                 ),
