265056cab078241802fba292b3f4481fc4507a34
osmin625 <tmdals179@gmali.com>
2023-07-26 13:36:32 +0000

    feat: dataset uploading is now possible

--- frontend/assets/sidebar.py
+++ frontend/assets/sidebar.py
@@ -1,10 +1,8 @@
-from dash import Input, Output, State, callback, dash_table, dcc, html
+from dash import Input, Output, State, callback, dcc, html
+import dash
 import dash_bootstrap_components as dbc
-import base64
-import datetime
 import io
 import pandas as pd
-
 
 sidebar_show = [
     dbc.Nav(
@@ -54,6 +52,15 @@ sidebar_show = [
                     "margin": "5% 3% 3% 10%",
                     "font-size": "15px",
                 },
+            ),
+            dcc.Store(
+                id="filename-store",
+            ),
+            dcc.Store(
+                id="date-store",
+            ),
+            dcc.Store(
+                id="data-store",
             ),
             html.Ul(id="file-list"),
             dbc.NavLink(
@@ -201,34 +208,25 @@ def toggle_sidebar(n, nclick):
     )
 
 
-def parse_contents(contents, filename, date):
-    content_type, content_string = contents.split(",")
-
-    decoded = base64.b64decode(content_string)
-    try:
-        if "csv" in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(io.StringIO(decoded.decode("utf-8")))
-        elif "xls" in filename:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-    except Exception as e:
-        print(e)
-        return html.Div(["There was an error processing this file."])
-
-    return html.Div(
-        [
-            html.H5(filename),
-            html.H6(datetime.datetime.fromtimestamp(date)),
-            dash_table.DataTable(
-                df.to_dict("records"), [{"name": i, "id": i} for i in df.columns]
-            ),
-            html.Hr(),  # horizontal line
-            # For debugging, display the raw contents provided by the web browser
-            html.Div("Raw Content"),
-            html.Pre(
-                contents[0:200] + "...",
-                style={"whiteSpace": "pre-wrap", "wordBreak": "break-all"},
-            ),
-        ]
-    )
+@callback(
+    Output("filename-store", "data"),
+    Output("date-store", "data"),
+    Output("data-store", "data"),
+    Input("upload-data", "contents"),
+    State("upload-data", "filename"),
+    State("upload-data", "last_modified"),
+)
+def store_output(content_string, filename, date):
+    if content_string is not None:
+        print("uploaded file: ", filename)
+        try:
+            if "csv" in filename:
+                df = pd.read_csv(io.StringIO(content_string))
+            elif "xls" in filename:
+                df = pd.read_excel(io.BytesIO(content_string))
+        except Exception as e:
+            print(e)
+            return "There was an error processing this file.", None, None
+        return filename, date, df.to_dict("records")
+    if content_string is None:
+        raise dash.exceptions.PreventUpdate
